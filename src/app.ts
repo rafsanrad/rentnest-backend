94cbcaba88959257829afd36f2f40cc7e5863d30
Rafsan Rad <rafsanrad@Rafsans-MacBook-Air.local>
2026-08-20 14:53:24 +0600

    initialized the project

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "RentNest API is running",
+  });
+});
+
+export default app;
